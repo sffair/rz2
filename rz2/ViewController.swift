01428fd0af86ba60ce2047e4ebffbc0aebf43a26
Fabrício Sperotto Sffair <Sperotto@Fabricios-MacBook-Pro.local>
2017-07-11 20:29:38 -0300

    Initial Commit

---
+++ rz2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rz2
+//
+//  Created by Fabrício Sperotto Sffair on 2017-07-11.
+//  Copyright © 2017 Fabrício Sperotto Sffair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
